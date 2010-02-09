d7796499ca12eb92e5d1c17332a419c94e06ee7b
evgenii.schepotiev <evgenii.schepotiev@746044e4-15bb-11df-830f-0d595e505705>
2010-02-09 22:54:14 +0000

    added list layout added icon

--- trunk/src/jetbrains/android/client/YouTrackActivity.java
+++ trunk/src/jetbrains/android/client/YouTrackActivity.java
@@ -1,7 +1,10 @@
 package jetbrains.android.client;
 
 import android.app.Activity;
+import android.app.ListActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.*;
 
 /**
  * Created by IntelliJ IDEA.
@@ -10,13 +13,24 @@ import android.os.Bundle;
  * Time: 1:28:22 AM
  * To change this template use File | Settings | File Templates.
  */
-public class YouTrackActivity extends Activity {
-    /**
-     * Called when the activity is first created.
-     */
+public class YouTrackActivity extends ListActivity {
     @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.main);
-    }
+       protected void onCreate(Bundle savedInstanceState) {
+
+           super.onCreate(savedInstanceState);    //To change body of overridden methods use File | Settings | File Templates.
+           setListAdapter(new ArrayAdapter<String>(this, R.layout.list_item, new String[]{"first", "second", "third"}));
+
+           ListView lv = getListView();
+           lv.setTextFilterEnabled(true);
+
+           lv.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+               public void onItemClick(AdapterView<?> parent, View view,
+                                       int position, long id) {
+                   // When clicked, show a toast with the TextView text
+                   Toast.makeText(getApplicationContext(), ((TextView) view).getText(),
+                           Toast.LENGTH_SHORT).show();
+               }
+           });
+       }
+
 }
