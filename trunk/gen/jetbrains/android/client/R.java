980e09e730165c2fa42a064020a6de677849dda8
maxim.mazin <maxim.mazin@746044e4-15bb-11df-830f-0d595e505705>
2010-02-16 22:08:57 +0000

    Improve list item layout, expand issue on selection

--- trunk/gen/jetbrains/android/client/R.java
+++ trunk/gen/jetbrains/android/client/R.java
@@ -14,14 +14,11 @@ public final class R {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int description=0x7f050003;
-        public static final int issueSelected=0x7f050000;
-        public static final int issue_id=0x7f050001;
-        public static final int summary=0x7f050002;
+        public static final int description=0x7f050001;
+        public static final int title=0x7f050000;
     }
     public static final class layout {
         public static final int issue_list_item=0x7f030000;
-        public static final int main=0x7f030001;
     }
     public static final class string {
         public static final int app_name=0x7f040001;
