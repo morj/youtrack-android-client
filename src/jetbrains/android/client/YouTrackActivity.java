07271ac81acc080a3e08b8fcea044e9b08c7a34d
maxim.mazin <maxim.mazin@746044e4-15bb-11df-830f-0d595e505705>
2010-03-27 12:45:17 +0000

    Make search work

--- src/jetbrains/android/client/YouTrackActivity.java
+++ src/jetbrains/android/client/YouTrackActivity.java
@@ -27,6 +27,7 @@ public class YouTrackActivity extends Activity {
     private final String[] from = new String[]{"title", "description"};
     private final int[] to = new int[]{R.id.title, R.id.description};
     private SimpleAdapter dataAdapter;
+    private String query;
 
 
     @Override
@@ -34,8 +35,15 @@ public class YouTrackActivity extends Activity {
         super.onCreate(savedInstanceState);
         // Load default values to preferences on app start
         PreferenceManager.setDefaultValues(this, R.xml.youtrack_prefrences, false);
+        login();
 
-        updateData(false);
+        if (Intent.ACTION_SEARCH.equals(getIntent().getAction())) {
+            query = getIntent().getStringExtra(SearchManager.QUERY);
+        } else {
+            query = PreferenceManager.getDefaultSharedPreferences(this).getString(getString(R.string.user_filter_preference), "");
+        }
+        updateQuery(false);
+
         dataAdapter = new SimpleAdapter(this, data, R.layout.issue_list_item, from, to);
         final ListView lv = new ListView(this);
 
@@ -58,8 +66,9 @@ public class YouTrackActivity extends Activity {
 
     @Override
     protected void onNewIntent(Intent intent) {
-        if(Intent.ACTION_SEARCH.equals(intent.getAction())){
-            updateQuery(true, intent.getStringExtra(SearchManager.QUERY));
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            this.query = intent.getStringExtra(SearchManager.QUERY);
+            updateQuery(true);
         }
     }
 
@@ -76,7 +85,7 @@ public class YouTrackActivity extends Activity {
                 finish();
                 return true;
             case R.id.update_item:
-                updateData(true);
+                updateQuery(true);
                 return true;
             case R.id.filter_item:
                 onSearchRequested();
@@ -93,10 +102,11 @@ public class YouTrackActivity extends Activity {
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
-         // The preferences returned if the request code is what we had given
+        // The preferences returned if the request code is what we had given
         // earlier in startSubActivity
-        if(requestCode == SUCCESS_CODE)
-            updateData(true);
+        if (requestCode == SUCCESS_CODE) {
+            login();
+        }
     }
 
     @Override
@@ -112,30 +122,28 @@ public class YouTrackActivity extends Activity {
         super.onDestroy();
     }
 
-    private void updateData(boolean notify) {
+    private void login() {
         SharedPreferences preferences = PreferenceManager.getDefaultSharedPreferences(this);
         try {
             String login = preferences.getString(getString(R.string.user_name_preference), null);
             String pass = preferences.getString(getString(R.string.user_password_preference), null);
             String host = preferences.getString(getString(R.string.host_url_preference_title), null);
             dao.login(host, login, pass);
-
-            String filter = preferences.getString(getString(R.string.user_filter_preference), null);
-            updateQuery(notify, filter);
         } catch (RequestFailedException e) {
             //TODO: Graceful handle
         }
     }
 
-    private void updateQuery(boolean notify, String filter) {
+    private void updateQuery(boolean notify) {
         data.clear();
         try {
-            data.addAll(dao.getIssues("JT", filter, 0, 10));
+            data.addAll(dao.getIssues("JT", query, 0, 10));
         } catch (RequestFailedException ignore) {
             //ignore
         }
-        if(notify)
+        if (notify) {
             dataAdapter.notifyDataSetChanged();
+        }
     }
 
 }
