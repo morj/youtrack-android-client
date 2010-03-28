8fc85a3e152f841f6f286889d1658248c66c2c87
maxim.mazin <maxim.mazin@746044e4-15bb-11df-830f-0d595e505705>
2010-03-28 12:59:03 +0000

    Special view for resolved issues in issue list

--- trunk/gen/jetbrains/android/client/R.java
+++ trunk/gen/jetbrains/android/client/R.java
@@ -10,52 +10,56 @@ package jetbrains.android.client;
 public final class R {
     public static final class attr {
     }
+    public static final class color {
+        public static final int priority_background=0x7f050001;
+        public static final int priority_normal=0x7f050000;
+    }
     public static final class drawable {
         public static final int icon=0x7f020000;
     }
     public static final class id {
-        public static final int description=0x7f070001;
-        public static final int filter_item=0x7f070007;
-        public static final int issue_comments=0x7f070004;
-        public static final int issue_description=0x7f070003;
-        public static final int issue_properties=0x7f070002;
-        public static final int options_item=0x7f070006;
-        public static final int quit_item=0x7f070008;
-        public static final int title=0x7f070000;
-        public static final int update_item=0x7f070005;
+        public static final int filter_item=0x7f080007;
+        public static final int issue_comments=0x7f080004;
+        public static final int issue_description=0x7f080003;
+        public static final int issue_id=0x7f080000;
+        public static final int issue_properties=0x7f080002;
+        public static final int options_item=0x7f080006;
+        public static final int quit_item=0x7f080008;
+        public static final int summary=0x7f080001;
+        public static final int update_item=0x7f080005;
     }
     public static final class layout {
         public static final int issue_list_item=0x7f030000;
         public static final int issue_properties=0x7f030001;
     }
     public static final class menu {
-        public static final int youtrack_menu=0x7f060000;
+        public static final int youtrack_menu=0x7f070000;
     }
     public static final class string {
-        public static final int app_name=0x7f050000;
-        public static final int host_url_preference_summary=0x7f050008;
-        public static final int host_url_preference_title=0x7f050007;
+        public static final int app_name=0x7f060000;
+        public static final int host_url_preference_summary=0x7f060008;
+        public static final int host_url_preference_title=0x7f060007;
         /** Menu Options
          */
-        public static final int options=0x7f050001;
-        public static final int query=0x7f050005;
-        public static final int quit=0x7f050002;
-        public static final int reports=0x7f050003;
-        public static final int search_hint=0x7f05000e;
+        public static final int options=0x7f060001;
+        public static final int query=0x7f060005;
+        public static final int quit=0x7f060002;
+        public static final int reports=0x7f060003;
+        public static final int search_hint=0x7f06000e;
         /**  Search 
          */
-        public static final int search_label=0x7f05000d;
+        public static final int search_label=0x7f06000d;
         /** Preferences
 Server Categoty
          */
-        public static final int server_preference_category=0x7f050006;
-        public static final int update=0x7f050004;
-        public static final int user_filter_preference=0x7f05000c;
-        public static final int user_name_preference=0x7f05000a;
-        public static final int user_password_preference=0x7f05000b;
+        public static final int server_preference_category=0x7f060006;
+        public static final int update=0x7f060004;
+        public static final int user_filter_preference=0x7f06000c;
+        public static final int user_name_preference=0x7f06000a;
+        public static final int user_password_preference=0x7f06000b;
         /** User Categoty
          */
-        public static final int user_preference_category=0x7f050009;
+        public static final int user_preference_category=0x7f060009;
     }
     public static final class xml {
         public static final int searchable=0x7f040000;
